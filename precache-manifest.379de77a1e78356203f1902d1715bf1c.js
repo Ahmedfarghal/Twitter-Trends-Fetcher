61913520155f40b2d5887e0840f1e829a33a8a3a
elharony <p.yahyaelharony@gmail.com>
2018-12-21 17:45:06 +0200

    Updates

---
+++ precache-manifest.379de77a1e78356203f1902d1715bf1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Twitter-Trend-Fetcher/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "917628b1419356189f5d",
+    "url": "/Twitter-Trend-Fetcher/static/js/runtime~main.917628b1.js"
+  },
+  {
+    "revision": "474b8744794037ebb1aa",
+    "url": "/Twitter-Trend-Fetcher/static/js/main.474b8744.chunk.js"
+  },
+  {
+    "revision": "88da6aaaf3d6c4263f87",
+    "url": "/Twitter-Trend-Fetcher/static/js/1.88da6aaa.chunk.js"
+  },
+  {
+    "revision": "474b8744794037ebb1aa",
+    "url": "/Twitter-Trend-Fetcher/static/css/main.4fcd2505.chunk.css"
+  },
+  {
+    "revision": "0ca01b8e8ea4f6a2dc34955d3036afe4",
+    "url": "/Twitter-Trend-Fetcher/index.html"
+  }
+];
