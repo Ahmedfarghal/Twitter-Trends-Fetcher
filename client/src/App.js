a80e611588a98730da3051b023a428827146ddb3
elharony <yahya@elharony.com>
2019-03-03 00:34:42 +0200

    Parse the 'response' data

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
   state = {
-    response: '',
+    response: [],
     post: '',
     responseToPost: '',
   };
+
   componentDidMount() {
     this.callApi()
-      .then(res => this.setState({ response: res.express }))
+      .then(res => {
+
+        // Parse Data [STRING into JSON]
+        const parsedData = JSON.parse(res.express)
+        const trends = parsedData["0"]["trends"]
+
+        // State Management
+        this.setState({ response: res.express })
+
+      })
       .catch(err => console.log(err));
   }
   callApi = async () => {
@@ -18,21 +29,19 @@ class App extends Component {
     if (response.status !== 200) throw Error(body.message);
     return body;
   };
-  handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('/api/world', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ post: this.state.post }),
-    });
-    const body = await response.text();
-    this.setState({ responseToPost: body });
-  };
+
+
+  
+
+
+
 render() {
     return (
       <div className="App">
+
+        <table id="datashow">
+        </table>
+
         <p>{this.state.response}</p>
         <form onSubmit={this.handleSubmit}>
           <p>
@@ -51,3 +60,47 @@ render() {
   }
 }
 export default App;
+
+
+
+/*
+
+[  
+  { 
+    "trends":[  
+        {  
+          "name":"Sergio Ramos",
+          "url":"http://twitter.com/search?q=%22Sergio+Ramos%22",
+          "promoted_content":null,
+          "query":"%22Sergio+Ramos%22",
+          "tweet_volume":55501
+        },
+        {  
+          "name":"#OhMyDad",
+          "url":"http://twitter.com/search?q=%23OhMyDad",
+          "promoted_content":null,
+          "query":"%23OhMyDad",
+          "tweet_volume":null
+        },
+        {  
+          "name":"#BernieInBrooklyn",
+          "url":"http://twitter.com/search?q=%23BernieInBrooklyn",
+          "promoted_content":null,
+          "query":"%23BernieInBrooklyn",
+          "tweet_volume":41189
+        }
+    ],
+
+    "as_of":"2019-03-02T21:46:46Z",
+    "created_at":"2019-03-02T21:45:47Z",
+    "locations":[  
+        {  
+          "name":"Worldwide",
+          "woeid":1
+        }
+    ]
+  }
+]
+
+
+*/
