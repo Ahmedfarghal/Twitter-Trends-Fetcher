3e47779e11cddd2708d809b7de04ab54482f420b
elharony <yahya@elharony.com>
2019-03-02 23:32:21 +0200

    Configure the API Configuration to get response

--- server.js
+++ server.js
@@ -5,9 +5,36 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
+// Twitter Fetch
+var trends = "NO DATA"
+updateTrends = (newTrends) => {
+    this.trends = newTrends
+    console.log(this.trends)
+}
+
+
+
+twitterFetch = async () => {
+    var Twit = require('twit')
+    var T = new Twit({
+        consumer_key: "hR8Ew7aaaWIdNPJFFI3mTyAPv",
+        consumer_secret: "treejdKGK0WGQJfnCdN3rpaI6tKPPIu7hna2OSR841MamXfXDI",
+        access_token: "2888180595-M7T0VQgObd74OTLaBwreZf6BPKJM1aN8iYgpCsX",
+        access_token_secret: "7Qsf2M5VNmsSTOKBXMafFi1BDHWMagy1SLsVtMOESJMOE",
+        timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
+        strictSSL: true // optional - requires SSL certificates to be valid.
+    });
+
+    T.get("trends/place", { id: 1, count: 2 }, function(err, data, response) {
+        console.log(JSON.stringify(data))
+        app.get('/api/hello', (req, res) => {
+            res.send({express: JSON.stringify(data)})
+        });
+    });
+}
+
+twitterFetch()
+
 app.post('/api/world', (req, res) => {
   console.log(req.body);
   res.send(
