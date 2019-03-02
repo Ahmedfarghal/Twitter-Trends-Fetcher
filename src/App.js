00f5493a7e9eed46e1933c97de0960a0bc378beb
elharony <yahya@elharony.com>
2019-03-02 18:19:13 +0200

    Add alternative solution structure

--- src/App.js
+++ src/App.js
@@ -3,6 +3,42 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    this.fetchTwitter()
+  }
+
+  fetchTwitter() {
+
+    //Callback functions
+    var error = function (err, response, body) {
+        console.log('ERROR [%s]', err);
+    };
+    var success = function (data) {
+        console.log('Data [%s]', data);
+    };
+
+    var config = {
+      "consumerKey": "hR8Ew7aaaWIdNPJFFI3mTyAPv",
+      "consumerSecret": "treejdKGK0WGQJfnCdN3rpaI6tKPPIu7hna2OSR841MamXfXDI",
+      "accessToken": "2888180595-M7T0VQgObd74OTLaBwreZf6BPKJM1aN8iYgpCsX",
+      "accessTokenSecret": "7Qsf2M5VNmsSTOKBXMafFi1BDHWMagy1SLsVtMOESJMOE",
+      "callBackUrl": "https://www.elharony.com/twitter-trend/"
+    }
+
+    var TwitApp = require('twitter-node-client').Twitter;
+
+    var twitter = new TwitApp(config);
+
+    twitter.getTweet({ id: '2888180595'}, error, success);
+  }
+
+
+
   render() {
     return (
       <div className="App">
